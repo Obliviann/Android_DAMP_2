9cdcf23a0e75c2259517fdf37f6fd07cc7ab67ce
Obliviann <34093004+Obliviann@users.noreply.github.com>
2019-10-22 22:50:23 +0200

    created ReciclerView obj, attached to LayoutMan(Linear) and Adapter(to be created)

--- app/src/main/java/com/liv/damp_2/UI/MovieList.kt
+++ app/src/main/java/com/liv/damp_2/UI/MovieList.kt
@@ -2,12 +2,37 @@ package com.liv.damp_2.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.liv.damp_2.R //La R son todos mis recursos de mi proyecto (ids, layouts...) como un array gigante
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.liv.damp_2.R //La R son todos los recursos de mi proyecto (ids, layouts...) como un array gigante
 
 class MovieList : AppCompatActivity() {
+    //Once we've added a RecyclerView widget to our layout file, we:
+    //1.obtain a handle to the object
+                        //var name      //type
+    private lateinit var recyclerView: RecyclerView
+    //2.connect it to a LayoutManager - provides the views to the object
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    /*3.attach an adapter -creates views for the data (items/ViewHolder objs) to be displayed
+                          -binds the VH to their data*/
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_list)
+                        // new LLM object
+        viewManager = LinearLayoutManager(this)
+        //viewAdapter = MovieAdapter(myDataset)                          //???
+                                    //casting
+        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
+            //setting to improve performance (use only if changes in content dont change layout size of the RV)
+            //setHasFixedSize(true)
+
+            //setLayoutManager to LinearLayouManager
+            layoutManager = viewManager
+
+            //setAdapter to MyAdapter
+            adapter = viewAdapter
+        }
     }
 }
