05b565bf7f4739823e001cc406d3f3a580713b41
Obliviann <34093004+Obliviann@users.noreply.github.com>
2019-10-23 23:29:16 +0200

    created Adapter and movie_item_view layout

--- app/src/main/java/com/liv/damp_2/UI/MovieAdapter.kt
+++ app/src/main/java/com/liv/damp_2/UI/MovieAdapter.kt
@@ -1,2 +1,43 @@
 package com.liv.damp_2.UI
 
+import androidx.recyclerview.widget.RecyclerView
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.widget.TextView
+import com.liv.damp_2.R
+
+//ADAPTER: created by extending RecyclerView.Adapter object
+//1. binds a VIEW to a DATA ITEM
+//1.2 (a complex data item may need more than one view, you provide access to all the views for a data item in a VIEW HOLDER)
+//2. creates VIEWHOLDERS objs (views) (layoutMan provides them to the obj)
+
+//TODO:MovieItem
+class MovieAdapter (private val movieDataset: Array<String>) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+    //1. We provide a reference to the VIEWS for each DATA ITEM
+    // Each data item is a String that is shown in a TextView??? TODO: String -> MovieItem?
+                                                                //TODO: create (TeaxtView) widget
+    class MovieViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+
+    //2. create a new VH (method invoked by the LayoutMan)             2.1
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieAdapter.MovieViewHolder {
+        val textView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.movie_item_view, parent, false) as TextView//2.2
+        //2.3 TODO: set the view's size, margins, paddings and layout parameters
+
+        return MovieViewHolder(textView)
+        //2.1 method needs to construct a RecyclerView.ViewHolder, whose type must match the type declared in the Adapter class signature
+        //2.2 it sets the view's contents by inflating an xml layout file TODO:create layout file
+        //2.3 me must assign these contents/data
+    }
+
+    //3. bind the VH to it's data/replace the contents of a V (method invoked by the LayoutMan)
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        // -TODO: get element from movieDataset at this position
+        // -TODO: replace the contents of the view with that element
+        holder.textView.text = movieDataset[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = movieDataset.size
+}
+
--- app/src/main/java/com/liv/damp_2/UI/MovieListActivity.kt
+++ app/src/main/java/com/liv/damp_2/UI/MovieListActivity.kt
@@ -6,32 +6,31 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.liv.damp_2.R //La R son todos los recursos de mi proyecto (ids, layouts...) como un array gigante
 
-class MovieListActivity : AppCompatActivity() {
+class MovieListActivity : AppCompatActivity() { //why 'AppCompat...'  ???
     //Once we've added a RecyclerView widget to our layout file, we:
     //1.obtain a handle to the object
-                        //var name      //type
     private lateinit var recyclerView: RecyclerView
     //2.connect it to a LayoutManager - provides the views to the object
     private lateinit var viewManager: RecyclerView.LayoutManager
-    /*3.attach an adapter -creates views for the data (items/ViewHolder objs) to be displayed
-                          -binds the VH to their data*/
+    //3.attach an adapter - binds the items/ViewHolder objs to their data, creates views
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_list)
-                        // new LLM object
+                      //new LLM object
         viewManager = LinearLayoutManager(this)
-        //viewAdapter = MovieAdapter(myDataset)                          //???
-                                    //casting
-        recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view).apply {
+        viewAdapter = MovieAdapter(movieDataset)                     //TODO: movieDataset???
+
+                                    /*<casting>*/
+        recyclerView = findViewById<RecyclerView>(R.id.movie_recycler_view).apply {
             //setting to improve performance (use only if changes in content dont change layout size of the RV)
             //setHasFixedSize(true)
 
-            //setLayoutManager to LinearLayouManager
+            //setLayoutManager to LinearLayoutManager
             layoutManager = viewManager
 
-            //setAdapter to MyAdapter
+            //setAdapter to MovieAdapter
             adapter = viewAdapter
         }
     }
